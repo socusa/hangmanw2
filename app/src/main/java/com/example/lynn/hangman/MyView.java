9ec7fc4e87c364a62196ab914dd03e964c8ecd59
Computer Lab <complab@asms.net>
2019-06-13 09:29:51 -0500

    Commit

--- app/src/main/java/com/example/lynn/hangman/MyView.java
+++ app/src/main/java/com/example/lynn/hangman/MyView.java
@@ -2,6 +2,7 @@ package com.example.lynn.hangman;
 
 import android.content.Context;
 import android.graphics.Point;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import static com.example.lynn.hangman.MainActivity.*;
@@ -21,6 +22,18 @@ public class MyView extends RelativeLayout {
         RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(2*width/3,3*height/4);
 
         buttonsView.setLayoutParams(params);
+
+        buttonsView.setId(View.generateViewId());
+
+        addView(buttonsView);
+
+        alphabetView = new AlphabetView(context);
+
+        params = new RelativeLayout.LayoutParams(2*width/3,height/4);
+
+        params.addRule(RelativeLayout.BELOW,)
+
+
 
     }
 
